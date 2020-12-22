daabc06810fe3262b5670bdb4a199263bedf5847
Alex Ivanov <aivanov@idtvision.com>
2020-12-22 14:31:34 -0800

    Added debug fs

--- drivers/dma/idt/vdm.c
+++ drivers/dma/idt/vdm.c
@@ -19,6 +19,7 @@
 #include <linux/slab.h>
 #include <linux/clk.h>
 #include <linux/io-64-nonatomic-lo-hi.h>
+#include <linux/debugfs.h>
 
 #include "../dmaengine.h"
 
@@ -29,10 +30,12 @@
  * struct vdm_device - DMA device structure
  * @reg: I/O  mapped base address
  * @dev: Device Structure
+ * @debugfs_dir: Debug FS directory ptr
  */
 struct vdm_device {
 	void __iomem *regs;
 	struct device *dev;
+	struct dentry *debugfs_dir;
 };
 
 static const struct of_device_id vdm_of_ids[] = {
@@ -40,6 +43,46 @@ static const struct of_device_id vdm_of_ids[] = {
 	{}
 };
 MODULE_DEVICE_TABLE(of, vdm_of_ids);
+
+static void idt_debugfs_remove(struct platform_device *pdev)
+{
+	struct vdm_device *vdev = platform_get_drvdata(pdev);
+        debugfs_remove_recursive(vdev->debugfs_dir);
+        vdev->debugfs_dir = NULL;
+}
+
+static int idt_debugfs_create(struct platform_device *pdev)
+{
+        const char *devnode;
+	int err;
+        char debugfs_dir[32];
+	struct vdm_device *vdev = platform_get_drvdata(pdev);
+
+        err = of_property_read_string(pdev->dev.of_node, "devnode", &devnode);
+        if (err) {
+                dev_err(&pdev->dev, "devnode not in DT\n");
+                return err;
+        }
+        snprintf(debugfs_dir, sizeof(debugfs_dir), "vdm-%s", devnode);
+
+        vdev->debugfs_dir = debugfs_create_dir(debugfs_dir, NULL);
+        if (vdev->debugfs_dir == NULL)
+                return -ENOMEM;
+
+#if 0
+	TODO: find out how to use this create file thing
+	if (!debugfs_create_file("streaming", 0644, priv->debugfs_dir, priv,
+                        &blade_debugfs_streaming_fops))
+                goto error;
+#endif
+
+        return 0;
+
+//error:
+        //idt_debugfs_remove(priv);
+
+        //return -ENOMEM;
+}
 
 /**
  * vdm_probe - Driver probe function
@@ -53,6 +96,7 @@ static int vdm_probe(struct platform_device *pdev)
 	struct vdm_device *vdev;
 	struct resource *io;
 	unsigned rev;
+	int err;
 
 	/* Allocate inst struct */
 	vdev = devm_kzalloc(&pdev->dev, sizeof(*vdev), GFP_KERNEL);
@@ -67,11 +111,19 @@ static int vdm_probe(struct platform_device *pdev)
 		return PTR_ERR(vdev->regs);
 
 	rev = ioread32(vdev->regs);
+	/* TODO: vdev is lost after this?; need to register it somewhere */
 	/* Check hardware revision */
 	if (rev != IDT_VDM_REV) {
 		dev_err(&pdev->dev, "Wrong HW version %d; support version is %d\n", rev, IDT_VDM_REV);
 		return -EFAULT;
 	}
+
+	platform_set_drvdata(pdev, vdev);
+
+	err = idt_debugfs_create(pdev);
+	if (err)
+		return err;
+
 	printk(KERN_INFO"Loaded vdm driver ver=%d\n", rev);
 	return 0;
 }
@@ -84,8 +136,9 @@ static int vdm_probe(struct platform_device *pdev)
  */
 static int vdm_remove(struct platform_device *pdev)
 {
-	of_dma_controller_free(pdev->dev.of_node);
-	printk(KERN_INFO"Removed vdm driver\n");
+	struct vdm_device *vdev = platform_get_drvdata(pdev);
+	idt_debugfs_remove(pdev);
+	printk(KERN_INFO"Removed vdm driver vdev=0x%p\n", vdev);
 	return 0;
 }
 
